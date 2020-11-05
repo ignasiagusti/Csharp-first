bb88cf3a5e67c3190af5f5167772d888c12c8b46
ignaagusti <ignasiagusti@gmail.com>
2020-11-05 15:28:37 +0100

    Exercici 3 uploaded

--- Program.cs
+++ Program.cs
@@ -9,7 +9,162 @@ namespace PrimeraAplicacion
         static void Main(string[] args)
         {
             //Exercici1();
-            Exercici2();
+            //Exercici2();
+            Exercici3();
+        }
+
+        static void Exercici3()
+        {
+            //FASE 1
+            List<string> cities = new List<string>() { "Barcelona", "Madrid", "Valencia", "Malaga", "Cadis", "Santander" };
+            string city1, city2, city3, city4, city5, city6;
+
+            /*for(int i=1; i<7; i++)
+            {
+                Console.WriteLine("Introdueix el nom de la ciutat {0}:",i);
+                string cit = Console.ReadLine();
+                cities.Add(cit);
+            }*/
+            city1 = cities[0];
+            city2 = cities[1];
+            city3 = cities[2];
+            city4 = cities[3];
+            city5 = cities[4];
+            city6 = cities[5];
+
+            Console.WriteLine("Exercici 1. Noms de les ciutats:");
+            Console.WriteLine($"{city1},{city2},{city3},{city4},{city5},{city6}");
+
+            //FASE 2
+
+            string[] arrayCities = new string[6];
+
+            arrayCities[0] = city1; //Ho faria amb un bucle i la llista de ciutats, pero suposo que s'ha de fer amb els strings
+            arrayCities[1] = city2;
+            arrayCities[2] = city3;
+            arrayCities[3] = city4;
+            arrayCities[4] = city5;
+            arrayCities[5] = city6;
+
+            for (int i = 0; i < arrayCities.Length; i++)
+            {
+                for (int j = 0; j < arrayCities.Length; j++)
+                {
+                    if (string.Compare(arrayCities[i], arrayCities[j]) < 0)
+                    {
+                        string var = arrayCities[i];
+                        arrayCities[i] = arrayCities[j];
+                        arrayCities[j] = var;
+                    }
+
+                    //Ho he provat comprovant cada caracter dels dos strings que comparem, però es molt mes eficient
+                    //amb el metode Compare d'string
+
+                    /*for(int k = 0; k < comp; k++)
+                    {
+                        if (str1[k].CompareTo(str2[k]) < 0)
+                        {
+                            string var = arrayCities[i];
+                            arrayCities[i] = arrayCities[j];
+                            arrayCities[j] = var;
+                        }
+                    }*/
+                }
+            }
+
+            Console.WriteLine("\nExercici2. Ciutats ordenades alfabèticament:");
+            foreach (string str in arrayCities)
+            {
+                Console.Write($"{str}, ");
+            }
+            Console.WriteLine("");
+
+            //FASE 3
+
+            string[] arrayModifCities = new string[6];
+
+            for (int i = 0; i < arrayCities.Length; i++)
+            {
+                string str = arrayCities[i].Replace('a', '4'); //Guardo en un string les ciutats amb la lletra a canviada
+                arrayModifCities[i] = str; //Introdueixo aquest string modificat a l'array de ciutats modificades
+            }
+
+            for (int i = 0; i < arrayModifCities.Length; i++) //Utilitzo el mateix bucle per ordenar
+            {
+                for (int j = 0; j < arrayModifCities.Length; j++)
+                {
+                    if (string.Compare(arrayModifCities[i], arrayModifCities[j]) < 0)
+                    {
+                        string var = arrayModifCities[i];
+                        arrayModifCities[i] = arrayModifCities[j];
+                        arrayModifCities[j] = var;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nExercici3. Ciutats modificades ordenades alfabèticament:");
+            foreach (string str in arrayModifCities)
+            {
+                Console.Write($"{str}, ");
+            }
+            Console.WriteLine("");
+
+            //FASE 4
+
+            /*No és possible inicialitzar els arrays amb aquests valors, ja que em diu que ha de ser un valor constant.*/
+            int[] lengths = new int[6] { city1.Length, city2.Length, city3.Length, city4.Length, city5.Length, city6.Length };
+            
+            char[] city1Array = new char[] { 'B', 'a', 'r', 'c', 'e', 'l', 'o', 'n', 'a' };
+            char[] city2Array = new char[] { 'M','a','d','r','i','d' };
+            char[] city3Array = new char[] { 'V','a','l','e','n','c','i','a' };
+            char[] city4Array = new char[] { 'M','a','l','a','g','a' };
+            char[] city5Array = new char[] { 'C','a','d','i','s' };
+            char[] city6Array = new char[] { 'S','a','n','t','a','n','d','e','r' };
+
+            //Per no haver de repetir bucles per cada array, creo mètode reverseOrder explicat més abaix i modifico cada array
+            city1Array = ReverseOrder(city1Array);
+            city2Array = ReverseOrder(city2Array);
+            city3Array = ReverseOrder(city3Array);
+            city4Array = ReverseOrder(city4Array);
+            city5Array = ReverseOrder(city5Array);
+            city6Array = ReverseOrder(city6Array);
+
+            Console.WriteLine("\nExercici 4. Arrays desordenats:");
+            //Crido al mètode per imprimir cada Array
+            PrintEx3(city1Array);
+            PrintEx3(city2Array);
+            PrintEx3(city3Array);
+            PrintEx3(city4Array);
+            PrintEx3(city5Array);
+            PrintEx3(city6Array);
+
+        }
+
+        /*Mètode per a inveritr l'ordre dels caràcters d'un array*/
+        static char[] ReverseOrder(char[] cityArray)
+        {
+            char[] changedArray = new char[cityArray.Length]; //Creo un array de la mateixa longitud que guardarà els
+                                                              // caràcters invertits d'ordre.
+            int pos = 0;
+            for (int i = cityArray.Length - 1; i >= 0; i--) //Començo a recòrrer l'array pel final per obtenir els últims caràcters primer
+            {
+                //Guardo cada caràcter a l'array temporal (cal utilitzar index que vagi del principi(0) al final,
+                //per aixo augmento la posició, ja que la nostra i va del final al principi.
+                changedArray[pos] = cityArray[i]; 
+                pos++;
+            }
+
+            return changedArray;
+        }
+
+        /*Mètode simple per imprimir els caràcters de l'array sense haver de repetir x cops el bucle.*/
+        static void PrintEx3(char[] cityArray)
+        {
+            foreach (char c in cityArray)
+            {
+                Console.Write(c);
+            }
+            Console.Write("\n");
         }
 
         static void Exercici2()
